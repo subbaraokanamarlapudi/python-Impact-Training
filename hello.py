0c4f61fe9da90a651c3194921e1bf2021927ba00
KANAMARLAPUDI VENKATA SUBBARAO <88045527+subbaraokanamarlapudi@users.noreply.github.com>
2023-06-03 23:03:23 +0530

    Add files via upload
    Basic Program.

---
+++ hello.py
@@ -0,0 +1,29 @@
+# Normal form.
+
+print("KVSR")
+print("CSE")
+
+#New lines.
+
+# print("Iam")
+# print("\n")
+# print("CSE student")
+
+#Sending Multiple Objects with Sep and End Parameters.
+
+# print('Hello','World',sep=' ',end='!!!')
+
+#Using String Concatenation.
+
+# string1 = "Hi "
+# string2 = "Hello "
+# string3 = "World"
+# string4 = "!!!"
+# print(string1 + string2 + string3 + string4)
+
+# Date and Time.
+
+# import datetime
+# now = datetime.datetime.now()
+# print(f'Current time is {now}')
+
