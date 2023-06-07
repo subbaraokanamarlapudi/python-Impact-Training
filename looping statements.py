dc6062659eaa8a5c133e239e4aeefd07744b5ba7
KANAMARLAPUDI VENKATA SUBBARAO <88045527+subbaraokanamarlapudi@users.noreply.github.com>
2023-06-07 10:20:54 +0530

    Add files via upload

---
+++ looping statements.py
@@ -0,0 +1,46 @@
+'''
+* Looping statements are called Iterative statements.
+* statements (or) elements are contionusly iterated.
+* for-loop and while-loop.
+
+* while-loop synatx:
+
+while condition:
+    statement.
+'''
+
+# for-loop
+for i in range(10):
+    print(i)
+
+# for i in range(0,10):
+#     print(i)
+
+# to print even numbers.
+# for i in range(0,10,2):
+#     print(i)
+
+# by using list.
+# a = [4,4,5,4,4,35,2332,3,55]
+# for i in (a):
+#     print(i)
+
+# by using string
+# b = "VIJAY"
+# for i in b:
+#     print(i)
+
+#-------------------------------- while-loop-------------------------#
+
+# while True:
+#     print("Hi")
+# output should be in infinte loop.
+
+# KVSR = 9
+# while KVSR<15:
+#     print("Hi",KVSR)
+#     KVSR+=1
+
+# for i in range(0,10):
+#     for j in range(0,100):
+#         print(i+j)
