fc0271525f56fa3b0f7bb50e54b95ce450e03db1
KANAMARLAPUDI VENKATA SUBBARAO <88045527+subbaraokanamarlapudi@users.noreply.github.com>
2023-06-07 10:21:15 +0530

    Add files via upload

---
+++ jumping statements.py
@@ -0,0 +1,31 @@
+'''
+* Jumping statements is used to jump the given condition.
+* There are 3 types:
+
+1.pass ------> Ignore.
+2.break -------> terminate.
+3.continue. -----> skip(current iteration)
+'''
+
+# pass
+
+# if True:
+#     pass
+
+# break statement
+
+# a = "VARISU"
+
+# for i in a:
+#     if i == 'R':
+#         break
+#     print(i)
+
+# # continue statement.
+
+# b = "VARISU"
+
+# for j in b:
+#     if j == 'R':
+#         continue
+#     print(j)
