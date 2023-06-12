690fc5329211f8a5b7cfa5fa28c365a7bf61dc81
KANAMARLAPUDI VENKATA SUBBARAO <88045527+subbaraokanamarlapudi@users.noreply.github.com>
2023-06-12 15:33:23 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,56 @@
+'''
+* It is also one datatype.
+* It  represented by "{}".
+* Do not allow the duplicates.
+* No index and it is a unordered set.
+* Do not allow mutable type as set of elements.
+
+SET METHODS:
+* add()
+* update()
+* pop()
+* remove()
+
+SET OPERATIONS:
+* union()
+* intersection()
+* difference()
+* issuperset()
+* issubset()
+
+'''
+
+s = {1,2,33,789,456,1000}
+print(type(s))
+print(s)
+
+# s = {2,42,25,66,900}
+# s.add(1000)
+# s.pop()
+# s.remove(42)
+# s.update({123,456,908})
+# print(s)
+
+# s1 = {1,2,3}
+# s2 = {4,5,6}
+
+# print(s1.union(s2))
+
+# s1 = {1,2,3,4,5}
+# s2 = {2,3,4,5,6,7}
+
+# print(s1.intersection(s2))
+
+# print(s1.difference(s2))
+# print(s2.difference(s1))
+
+# s1 = {1,2,3,4,5,6}
+# s2 = {1,2,3}
+
+# print(s1.issuperset(s2))
+# print(s2.issubset(s1))
+# print(s1.isdisjoint(s2))
+# print(s2.isdisjoint(s1))
+
+# for i in (1,2,3,4):
+#     print(i)
